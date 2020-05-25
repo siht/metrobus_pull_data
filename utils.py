af464b11cdf29dad260ed3198884625a69ad8201
siht <carlos.tz.00.00@gmail.com>
2020-05-25 17:07:24 -0500

    add function that creates new metrobus register

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+import pandas as pd
+
 __all__ = (
     'construct_engine_string',
+    'create_metrobuses_if_doesnt_exist',
+    'create_places_if_doesnt_exist',
 )
 
 def construct_engine_string(db_config):
@@ -13,3 +17,31 @@ def construct_engine_string(db_config):
         port = db_config['PORT'],
         database = db_config['NAME'],
     )
+
+def create_metrobuses_if_doesnt_exist(engine, data):
+    '''
+    create new metrobus registers in db if contains some new
+    '''
+    df_saved_metrobuses = (
+        pd.read_sql_table('metrobus_history_metrobus', engine)
+    )
+    df_fetched_metrobuses = (
+        pd.Series(map(lambda elem: elem['serie'], data)).to_frame('serie')
+    )
+    df_new_metrobuses = (
+        df_saved_metrobuses
+        .merge(df_fetched_metrobuses, on='serie', how='right')
+    )
+    theres_new_metrobuses = not df_new_metrobuses.empty
+    if(theres_new_metrobuses):
+        last_id = (
+            not df_saved_metrobuses.empty
+            and df_saved_metrobuses.groupby('id').tail(1)
+            or -1
+        )
+        current_id = last_id + 1
+        next_new_id = current_id + len(df_new_metrobuses)
+        new_ids = pd.Series(range(current_id, next_new_id))
+        df_new_metrobuses['id'] = new_ids
+        df_new_metrobuses = df_new_metrobuses.set_index('id')
+        df_new_metrobuses.to_sql('metrobus_history_metrobus', engine, if_exists='append')
