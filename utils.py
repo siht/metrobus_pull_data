17bf7c1cde8e86e75091db76374e562e71297fdf
siht <carlos.tz.00.00@gmail.com>
2020-05-26 16:54:52 -0500

    lint code

--- utils.py
+++ utils.py
@@ -5,17 +5,21 @@ __all__ = (
     'create_historical_points',
 )
 
+
 def construct_engine_string(db_config):
     '''
     recibe a dict and return a string
     '''
-    return "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(
-        user = db_config['USER'],
-        password = db_config['PASSWORD'],
-        host = db_config['HOST'],
-        port = db_config['PORT'],
-        database = db_config['NAME'],
-    )
+    return (
+        "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+        ).format(
+            user=db_config['USER'],
+            password=db_config['PASSWORD'],
+            host=db_config['HOST'],
+            port=db_config['PORT'],
+            database=db_config['NAME'],
+        )
+
 
 def create_metrobuses_if_doesnt_exist(engine, data):
     '''
@@ -33,9 +37,15 @@ def create_metrobuses_if_doesnt_exist(engine, data):
         df_saved_metrobuses
         .merge(df_fetched_metrobuses, on='serie', how='right')
     )
-    df_new_metrobuses = df_review_metrobuses[pd.isnull(df_review_metrobuses['id'])]
-    df_already_registered_metrobuses = df_review_metrobuses.dropna(subset=['id'])
-    df_already_registered_metrobuses = df_already_registered_metrobuses.set_index('id')
+    df_new_metrobuses = (
+        df_review_metrobuses[pd.isnull(df_review_metrobuses['id'])]
+    )
+    df_already_registered_metrobuses = (
+        df_review_metrobuses.dropna(subset=['id'])
+    )
+    df_already_registered_metrobuses = (
+        df_already_registered_metrobuses.set_index('id')
+    )
     theres_new_metrobuses = not df_new_metrobuses.empty
     if(theres_new_metrobuses):
         last_id = (
@@ -50,9 +60,16 @@ def create_metrobuses_if_doesnt_exist(engine, data):
         new_ids = pd.Series(range(current_id, next_new_id))
         df_new_metrobuses['id'] = new_ids
         df_new_metrobuses = df_new_metrobuses.set_index('id')
-        df_new_metrobuses.to_sql('metrobus_history_metrobus', engine, if_exists='append')
-        return pd.concat((df_already_registered_metrobuses, df_new_metrobuses,))
+        df_new_metrobuses.to_sql(
+            'metrobus_history_metrobus',
+            engine,
+            if_exists='append'
+        )
+        return pd.concat(
+            (df_already_registered_metrobuses, df_new_metrobuses,)
+        )
     return df_already_registered_metrobuses
+
 
 def create_places_if_doesnt_exist(engine, data):
     '''
@@ -106,13 +123,18 @@ def create_places_if_doesnt_exist(engine, data):
         new_ids = pd.Series(range(current_id, next_new_id))
         df_new_places['id'] = new_ids
         df_new_places = df_new_places.set_index('id')
-        df_new_places.to_sql('metrobus_history_place', engine, if_exists='append')
+        df_new_places.to_sql(
+            'metrobus_history_place',
+            engine,
+            if_exists='append'
+        )
         return pd.concat((df_new_places, df_already_registered_places,))
     return df_already_registered_places
 
+
 def create_historical_points(engine, data):
     '''
-    create new historical points, and create metrobuses registries 
+    create new historical points, and create metrobuses registries
     and/or places registries if doesnt exist
     '''
     df_metrobuses = create_metrobuses_if_doesnt_exist(engine, data)
@@ -122,18 +144,36 @@ def create_historical_points(engine, data):
     df_origin_data = pd.DataFrame(data)
 
     df_origin_data = df_origin_data.merge(df_metrobuses, on='serie')
-    df_origin_data = df_origin_data.merge(df_places, on=['latitude', 'longitude'])
-    df_origin_data.drop(['serie', 'latitude', 'longitude'], axis=1, inplace=True)
+    df_origin_data = (
+        df_origin_data.merge(df_places, on=['latitude', 'longitude'])
+    )
+    df_origin_data.drop(
+        ['serie', 'latitude', 'longitude'],
+        axis=1,
+        inplace=True
+    )
     last_id_historical_point = (
         engine.execute(
             'SELECT id FROM metrobus_history_historicalpoint'
             ' ORDER BY id DESC LIMIT 1;'
         ).fetchone()
     )
-    last_id_historical_point = last_id_historical_point[0] if last_id_historical_point else -1
+    last_id_historical_point = (
+        last_id_historical_point[0] if last_id_historical_point else -1
+    )
     current_id_historical_point = last_id_historical_point + 1
-    next_id_historical_point = current_id_historical_point + df_origin_data.count()
-    new_ids = pd.Series(range(current_id_historical_point, next_id_historical_point))
+    next_id_historical_point = (
+        current_id_historical_point + df_origin_data.count()
+    )
+    new_ids = (
+        pd.Series(
+            range(current_id_historical_point, next_id_historical_point)
+        )
+    )
     df_origin_data['id'] = new_ids
     df_origin_data = df_origin_data.set_index('id')
-    df_origin_data.to_sql('metrobus_history_historicalpoint', engine, if_exists='append')
+    df_origin_data.to_sql(
+        'metrobus_history_historicalpoint',
+        engine,
+        if_exists='append'
+    )
